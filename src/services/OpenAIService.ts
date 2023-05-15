1b9106245684e28a6abc19bc2b22d2591caf65fa
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2023-05-15 17:20:34 +0300

    feat: add openaiservice

--- src/services/OpenAIService.ts
+++ src/services/OpenAIService.ts
@@ -1,66 +1,36 @@
 import { isAxiosError } from '../types/typeGuards';
-import {
-  GoalDescription,
-  ParsedCompanyDetails,
-  ResponsePayload,
-} from '@/types';
+import { Description, ResponsePayload } from '@/types';
 import axios, { AxiosError } from 'axios';
 
 const DEFAULT_ERROR_MESSAGE =
   'The AI service is experiencing high traffic. Please try again.';
 
 const fetcher = axios.create({
-  baseURL: 'https://dev-api-10101.greenifs.com/api/greenGoalsAI',
+  // baseURL: 'https://dev-api-10101.greenifs.com/api/sustainabilityMarketing',
+  baseURL: 'http://localhost:5000/api/sustainabilityMarketing',
   method: 'POST',
 });
 
 export class OpenAIApi {
-  public static getGoalsByCompanyDetails = async (
-    companyDetails: ParsedCompanyDetails
-  ): Promise<ResponsePayload<Array<string>>> => {
+  public static getAssistedBySustainabilityMarketing = async (
+    description: Description
+  ): Promise<ResponsePayload<string>> => {
     try {
-      const response = await fetcher.post('/getGoalsByCompanyDetails', {
-        company: {
-          companyName: companyDetails.companyName,
-          industry: companyDetails.industry,
-          country: companyDetails.country,
-          companySize: companyDetails.companySize,
-        },
-      });
-      console.log(response);
-      return { result: response.data };
+      const response = await fetcher.post(
+        '/getAssistedBySustainabilityMarketing',
+        {
+          details: {
+            description: description.description,
+          },
+        }
+      );
+      console.log(response.data);
+      return { result: response.data.result };
     } catch (error: unknown) {
-      console.error('OpenAIService.getGoalsByCompanyDetails: ', error);
-      if (!isAxiosError(error)) return { error: DEFAULT_ERROR_MESSAGE };
-      let errorMessage = DEFAULT_ERROR_MESSAGE;
-      if (error.response?.status === 408) {
-        errorMessage =
-          'The AI service is experiencing high traffic. Please try again.';
-      }
-      if (error.response?.status === 429) {
-        errorMessage = 'Too many requests. Please try again later.';
-      }
-      return { error: errorMessage };
-    }
-  };
-
-  public static getDescriptionsByGoals = async (
-    goals: Array<string>,
-    companyDetails: ParsedCompanyDetails
-  ): Promise<ResponsePayload<Array<GoalDescription>>> => {
-    try {
-      const response = await fetcher.post('/getDescriptionsByGoals', {
-        companyDetails: {
-          companyName: companyDetails.companyName,
-          industry: companyDetails.industry,
-          country: companyDetails.country,
-          companySize: companyDetails.companySize,
-        },
-        goals,
-      });
-      return { result: response.data };
-    } catch (error: unknown) {
-      console.error('OpenAIService.getDescriptionsByGoals: ', error);
+      console.error(
+        'OpenAIService.getAssistedBySustainabilityMarketing: ',
+        error
+      );
       if (!isAxiosError(error)) return { error: DEFAULT_ERROR_MESSAGE };
       let errorMessage = DEFAULT_ERROR_MESSAGE;
       if (error.response?.status === 408) {
