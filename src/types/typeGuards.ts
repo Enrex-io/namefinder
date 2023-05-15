63a60328fdacf98a6156f94cad4335588a959997
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2023-05-15 17:21:14 +0300

    feat: add isDescription

--- src/types/typeGuards.ts
+++ src/types/typeGuards.ts
@@ -1,16 +1,9 @@
-import { CompanyDetails, CompanyDetailsWithGoals } from ".";
-import { AxiosError } from "axios";
+import { AxiosError } from 'axios';
+import { Description } from '.';
 
-export const isCompanyDetails = (body: any): body is CompanyDetails => {
-  const { companyName, industry, country, companySize } = body;
-  return !!companyName && !!industry && !!country && !!companySize;
-};
-
-export const isCompanyDetailsWithGoals = (
-  body: any
-): body is CompanyDetailsWithGoals => {
-  const { goals, companyDetails } = body;
-  return isCompanyDetails(companyDetails) && goals?.length;
+export const isDescription = (body: any): body is Description => {
+  const { description } = body;
+  return !!description;
 };
 
 export const isAxiosError = (error: any): error is AxiosError => {
