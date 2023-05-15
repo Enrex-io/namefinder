9222d20eab0cbe7a793bda675b57b51d3edde133
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2023-05-15 17:21:01 +0300

    feat: add parsedDescription

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,28 +1,11 @@
-import { CompanySizes } from '@/consts/companySizes';
-import { countries } from '@/consts/countries';
-import { ParsedCompanyDetails } from '@/types';
-
-export const shuffleArray = (array: Array<unknown>) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
+import { Description } from '@/types';
 
 export const delay = (fn: Function, ms: number, ...args: Array<unknown>) => {
   return setTimeout(fn, ms, ...args);
 };
 
-export const parseCompanyDetails = (values: Record<string, any>) : ParsedCompanyDetails => {
+export const parsedescription = (values: Record<string, any>): Description => {
   return {
-    companyName: values.companyName,
-    industry: values.sectorAndIndustry,
-    country:
-      countries.find((country) => country.code === values.country)?.label ||
-      values.country,
-    companySize:
-      CompanySizes[values.companySize as keyof typeof CompanySizes] ||
-      values.companySize,
+    description: values.description,
   };
-}
+};
