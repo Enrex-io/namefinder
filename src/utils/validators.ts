a15369c9622d952852a23040c509dee40497b810
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2023-05-16 14:30:13 +0300

    feat: add description validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -3,7 +3,8 @@ import { EMAIL_VALIDATION_REGEX } from '@/consts/regex';
 export const validateDescription = (value: string) => {
   if (!value) return 'Description is required';
   if (value.length < 3)
-    return "Description doesn't look like a realDescriptione";
+    return "Description doesn't look like a realDescription";
+  if (value.length > 2000) return 'Description is too big';
 };
 
 export const validateEmail = (value: string) => {
