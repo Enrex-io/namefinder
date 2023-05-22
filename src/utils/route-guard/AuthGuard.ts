d7279fcb3e898325665273860588acd6e98d7693
Fiodar Savutsin <rabinzonim@gmail.com>
2023-05-22 18:19:57 +0300

    feat: add boilerplate for AuthGuard

--- src/utils/route-guard/AuthGuard.ts
+++ src/utils/route-guard/AuthGuard.ts
@@ -1,21 +1,21 @@
-// import { ReactElement, useEffect } from 'react';
-// import { useRouter } from 'next/router';
-// import { useAuth } from '../../hooks/useAuth';
+import { ReactElement, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../../hooks/useAuth';
 
-// const AuthGuard = ({ children }: { children: ReactElement | null }) => {
-//   const { user } = useAuth();
-//   const isLoggedIn = user?.isLoggedIn;
-//   const router = useRouter();
-//   useEffect(() => {
-//     if (!isLoggedIn) {
-//       router.push('/');
-//     }
-//     // eslint-disable-next-line
-//   }, [isLoggedIn]);
+const AuthGuard = ({ children }: { children: ReactElement | null }) => {
+  const { user } = useAuth();
+  const isLoggedIn = user ? Boolean(user.id) : false;
+  const router = useRouter();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push('/');
+    }
+    // eslint-disable-next-line
+  }, [isLoggedIn]);
 
-//   if (!isLoggedIn) return 'Loading...';
+  if (!isLoggedIn) return 'Loading...';
 
-//   return children;
-// };
+  return children;
+};
 
-// export default AuthGuard;
+export default AuthGuard;
