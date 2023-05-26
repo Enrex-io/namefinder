830c40d1ed78f96df538da579e861df79ea91d17
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2023-05-26 10:34:46 +0300

    feat: add IPrompt

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,6 @@
+import Regions from "@/consts/region";
+import Medias from "@/consts/medias";
+
 export type Phone = `${number}` | `${number}-${number}`;
 export type Label = Capitalize<string>;
 
@@ -47,3 +50,14 @@ export type UserProfile = {
   token?: string,
   claims?: { [key: string]: any };
 };
+
+export interface IPrompt {
+  userId: string;
+  media: Medias;
+  region: Regions;
+  request: string;
+  response: {
+    terms: string[];
+    correctText: string;
+  }
+}
