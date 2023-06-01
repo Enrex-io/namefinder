694a5c52a5b120085d0c838ddd20459277461076
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-01 13:30:32 +0300

    refactor: remove old page styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app';
 import { fontInter } from '@/styles/fonts';
 import '@/styles/globals.scss';
 import { FirebaseProvider as AuthProvider } from '../contexts/FirebaseContext';
-import './login.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
