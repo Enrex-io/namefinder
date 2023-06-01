bc15cb9e993a00b3fc922c80322d603d8afdd4ad
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2023-06-01 17:57:57 +0300

    Revert "fix: reveted commits"
    This reverts commit f860131d2f5246519f1059567cece80335b711da.

--- src/utils/route-guard/AuthGuard.tsx
+++ src/utils/route-guard/AuthGuard.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import useAuth from '../../hooks/useAuth';
-import FullscreenLoader from '@/components/Loader/FullscreenLoader';
+import Loader from '@/components/Loader/Loader';
 
 const AuthGuard = ({ children }: { children: ReactElement | null }) => {
     const { user } = useAuth();
@@ -16,7 +16,19 @@ const AuthGuard = ({ children }: { children: ReactElement | null }) => {
     }, [isLoggedIn]);
 
     if (!isLoggedIn) {
-        return <FullscreenLoader />;
+        return (
+            <div
+                style={{
+                    height: '100vh',
+                    width: '100vw',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Loader height={21} />
+            </div>
+        );
     }
 
     return children;
