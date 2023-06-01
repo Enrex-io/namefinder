1b207c4190cae03363d7a4cbc26a1ad2199000ad
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-01 13:30:32 +0300

    refactor: remove old page styles

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head';
 import { META } from '@/consts/meta';
-import classes from './login.module.scss';
-import { useEffect } from 'react';
-import useAuth from '@/hooks/useAuth';
-import { useRouter } from 'next/router';
-import { GreenWashingUserService } from '@/services/GreenWashingUserService';
 import Link from 'next/link';
 import Logo from '@/components/Logo/Logo';
 import AuthLogin from '@/components/authentication/auth-forms/AuthLogin';
@@ -28,118 +23,114 @@ export default function Login() {
                     content="width=device-width, initial-scale=1"
                 />
             </Head>
-            <div className={classes.container}>
-                <AuthWrapper1>
-                    <Grid
-                        container
-                        direction="column"
-                        justifyContent="flex-end"
-                        sx={{ minHeight: '100vh' }}
-                    >
-                        <Grid item xs={12}>
-                            <Grid
-                                container
-                                justifyContent="center"
-                                alignItems="center"
-                                sx={{ minHeight: 'calc(100vh - 68px)' }}
-                            >
-                                <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
-                                    <AuthCardWrapper>
-                                        <Grid
-                                            container
-                                            spacing={2}
-                                            alignItems="center"
-                                            justifyContent="center"
-                                        >
-                                            <Grid item sx={{ mb: 3 }}>
-                                                <Link href="#">
-                                                    <Logo />
-                                                </Link>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Grid
-                                                    container
-                                                    direction={
-                                                        matchDownSM
-                                                            ? 'column-reverse'
-                                                            : 'row'
-                                                    }
-                                                    alignItems="center"
-                                                    justifyContent="center"
-                                                >
-                                                    <Grid item>
-                                                        <Stack
-                                                            alignItems="center"
-                                                            justifyContent="center"
-                                                            spacing={1}
-                                                        >
-                                                            <Typography
-                                                                color="#009688;"
-                                                                gutterBottom
-                                                                variant={
-                                                                    matchDownSM
-                                                                        ? 'h3'
-                                                                        : 'h2'
-                                                                }
-                                                            >
-                                                                Hi, Welcome Back
-                                                            </Typography>
-                                                            <Typography
-                                                                variant="caption"
-                                                                fontSize="16px"
-                                                                textAlign={
-                                                                    matchDownSM
-                                                                        ? 'center'
-                                                                        : 'inherit'
-                                                                }
-                                                            >
-                                                                Enter your
-                                                                credentials to
-                                                                continue
-                                                            </Typography>
-                                                        </Stack>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <AuthLogin />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Divider />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Grid
-                                                    item
-                                                    container
-                                                    direction="column"
-                                                    alignItems="center"
-                                                    xs={12}
-                                                >
-                                                    <Typography
-                                                        component={Link}
-                                                        href="/register"
-                                                        variant="subtitle1"
-                                                        sx={{
-                                                            textDecoration:
-                                                                'none',
-                                                        }}
+            <AuthWrapper1>
+                <Grid
+                    container
+                    direction="column"
+                    justifyContent="flex-end"
+                    sx={{ minHeight: '100vh' }}
+                >
+                    <Grid item xs={12}>
+                        <Grid
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                            sx={{ minHeight: 'calc(100vh - 68px)' }}
+                        >
+                            <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                                <AuthCardWrapper>
+                                    <Grid
+                                        container
+                                        spacing={2}
+                                        alignItems="center"
+                                        justifyContent="center"
+                                    >
+                                        <Grid item sx={{ mb: 3 }}>
+                                            <Link href="#">
+                                                <Logo />
+                                            </Link>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Grid
+                                                container
+                                                direction={
+                                                    matchDownSM
+                                                        ? 'column-reverse'
+                                                        : 'row'
+                                                }
+                                                alignItems="center"
+                                                justifyContent="center"
+                                            >
+                                                <Grid item>
+                                                    <Stack
+                                                        alignItems="center"
+                                                        justifyContent="center"
+                                                        spacing={1}
                                                     >
-                                                        Don&apos;t have an
-                                                        account?
-                                                    </Typography>
+                                                        <Typography
+                                                            color="#009688;"
+                                                            gutterBottom
+                                                            variant={
+                                                                matchDownSM
+                                                                    ? 'h3'
+                                                                    : 'h2'
+                                                            }
+                                                        >
+                                                            Hi, Welcome Back
+                                                        </Typography>
+                                                        <Typography
+                                                            variant="caption"
+                                                            fontSize="16px"
+                                                            textAlign={
+                                                                matchDownSM
+                                                                    ? 'center'
+                                                                    : 'inherit'
+                                                            }
+                                                        >
+                                                            Enter your
+                                                            credentials to
+                                                            continue
+                                                        </Typography>
+                                                    </Stack>
                                                 </Grid>
                                             </Grid>
                                         </Grid>
-                                    </AuthCardWrapper>
-                                </Grid>
+                                        <Grid item xs={12}>
+                                            <AuthLogin />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Divider />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Grid
+                                                item
+                                                container
+                                                direction="column"
+                                                alignItems="center"
+                                                xs={12}
+                                            >
+                                                <Typography
+                                                    component={Link}
+                                                    href="/register"
+                                                    variant="subtitle1"
+                                                    sx={{
+                                                        textDecoration: 'none',
+                                                    }}
+                                                >
+                                                    Don&apos;t have an account?
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </AuthCardWrapper>
                             </Grid>
                         </Grid>
-                        <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
-                            <AuthFooter />
-                        </Grid>
                     </Grid>
-                </AuthWrapper1>
-            </div>
+                    <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+                        <AuthFooter />
+                    </Grid>
+                </Grid>
+            </AuthWrapper1>
         </>
     );
 }
