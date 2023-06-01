934c7cd5dd923aecd573090740b6296bc8a49465
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-01 15:21:27 +0300

    feat: add SnackbarContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app';
 import { fontInter } from '@/styles/fonts';
 import '@/styles/globals.scss';
 import { FirebaseProvider as AuthProvider } from '../contexts/FirebaseContext';
+import { SnackbarProvider } from '@/contexts/SnackbarContext';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <main className={fontInter.className}>
             <AuthProvider>
-                <Component {...pageProps} />
+                <SnackbarProvider>
+                    <Component {...pageProps} />
+                </SnackbarProvider>
             </AuthProvider>
         </main>
     );
