6f10447605130e379a4c01a51592c941840b8a3f
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-01 13:23:57 +0300

    feat: move login page from gi-dashboard

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,69 +1,21 @@
-import firebase from '../firebase';
-import 'firebaseui/dist/firebaseui.css';
 import Head from 'next/head';
 import { META } from '@/consts/meta';
 import classes from './login.module.scss';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import useAuth from '@/hooks/useAuth';
 import { useRouter } from 'next/router';
 import { GreenWashingUserService } from '@/services/GreenWashingUserService';
-import firebaseui from 'firebaseui';
 import Link from 'next/link';
 import Logo from '@/components/Logo/Logo';
+import AuthLogin from '@/components/authentication/auth-forms/AuthLogin';
+import AuthWrapper1 from '@/components/authentication/AuthWrapper1';
+import Grid from '@mui/material/Grid';
+import AuthCardWrapper from '@/components/authentication/AuthCardWrapper';
+import { Divider, Stack, Typography, useMediaQuery } from '@mui/material';
+import AuthFooter from '@/components/authentication/AuthFooter';
 
-export default function Home() {
-    const firebaseAuthContainerRef = useRef<HTMLDivElement | null>(null);
-    const ui = useRef<firebaseui.auth.AuthUI>();
-    const router = useRouter();
-    const { user } = useAuth();
-
-    useEffect(() => {
-        async function createUserThenRedirect() {
-            if (user) {
-                await GreenWashingUserService.createUser();
-                router.push('/');
-            }
-        }
-
-        createUserThenRedirect();
-    }, [user, router]);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            import('firebaseui').then((firebaseui) => {
-                if (!ui.current && firebaseAuthContainerRef.current) {
-                    ui.current =
-                        firebaseui.auth.AuthUI.getInstance() ||
-                        new firebaseui.auth.AuthUI(firebase.auth());
-                    ui.current.start(firebaseAuthContainerRef.current, {
-                        callbacks: {
-                            signInSuccessWithAuthResult: function () {
-                                //We will manually redirect after ensuring that the user is present in the context to avoid bugs with redirects
-                                return false;
-                            },
-                        },
-                        signInOptions: [
-                            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                            {
-                                provider:
-                                    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                                signInMethod:
-                                    firebase.auth.EmailAuthProvider
-                                        .EMAIL_LINK_SIGN_IN_METHOD,
-                                forceSameDevice: false,
-                                emailLinkSignIn: function () {
-                                    return {
-                                        url: window.location.href,
-                                    };
-                                },
-                            },
-                        ],
-                    });
-                }
-            });
-        }
-    }, []);
+export default function Login() {
+    const matchDownSM = useMediaQuery('(min-width:900px)');
 
     return (
         <>
@@ -77,13 +29,116 @@ export default function Home() {
                 />
             </Head>
             <div className={classes.container}>
-                <div className={classes.loginPaper}>
-                    <Link href="#">
-                        <Logo />
-                    </Link>
-                    <p className={classes.signInText}>Sign in to continue</p>
-                    <div ref={firebaseAuthContainerRef} />
-                </div>
+                <AuthWrapper1>
+                    <Grid
+                        container
+                        direction="column"
+                        justifyContent="flex-end"
+                        sx={{ minHeight: '100vh' }}
+                    >
+                        <Grid item xs={12}>
+                            <Grid
+                                container
+                                justifyContent="center"
+                                alignItems="center"
+                                sx={{ minHeight: 'calc(100vh - 68px)' }}
+                            >
+                                <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                                    <AuthCardWrapper>
+                                        <Grid
+                                            container
+                                            spacing={2}
+                                            alignItems="center"
+                                            justifyContent="center"
+                                        >
+                                            <Grid item sx={{ mb: 3 }}>
+                                                <Link href="#">
+                                                    <Logo />
+                                                </Link>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Grid
+                                                    container
+                                                    direction={
+                                                        matchDownSM
+                                                            ? 'column-reverse'
+                                                            : 'row'
+                                                    }
+                                                    alignItems="center"
+                                                    justifyContent="center"
+                                                >
+                                                    <Grid item>
+                                                        <Stack
+                                                            alignItems="center"
+                                                            justifyContent="center"
+                                                            spacing={1}
+                                                        >
+                                                            <Typography
+                                                                color="#009688;"
+                                                                gutterBottom
+                                                                variant={
+                                                                    matchDownSM
+                                                                        ? 'h3'
+                                                                        : 'h2'
+                                                                }
+                                                            >
+                                                                Hi, Welcome Back
+                                                            </Typography>
+                                                            <Typography
+                                                                variant="caption"
+                                                                fontSize="16px"
+                                                                textAlign={
+                                                                    matchDownSM
+                                                                        ? 'center'
+                                                                        : 'inherit'
+                                                                }
+                                                            >
+                                                                Enter your
+                                                                credentials to
+                                                                continue
+                                                            </Typography>
+                                                        </Stack>
+                                                    </Grid>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <AuthLogin />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Divider />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Grid
+                                                    item
+                                                    container
+                                                    direction="column"
+                                                    alignItems="center"
+                                                    xs={12}
+                                                >
+                                                    <Typography
+                                                        component={Link}
+                                                        href="/register"
+                                                        variant="subtitle1"
+                                                        sx={{
+                                                            textDecoration:
+                                                                'none',
+                                                        }}
+                                                    >
+                                                        Don&apos;t have an
+                                                        account?
+                                                    </Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </AuthCardWrapper>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+                            <AuthFooter />
+                        </Grid>
+                    </Grid>
+                </AuthWrapper1>
             </div>
         </>
     );
