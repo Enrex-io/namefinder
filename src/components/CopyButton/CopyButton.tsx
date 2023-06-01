a71537a95203f9f0cbbcb80a7676835f6d16e4c6
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2023-06-01 14:27:55 +0300

    feat: add to copy btn css and icon

--- src/components/CopyButton/CopyButton.tsx
+++ src/components/CopyButton/CopyButton.tsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from 'react';
 import Button from '../Button/Button';
+import Image from 'next/image';
+import classes from './CopyButton.module.scss';
 
 interface Props {
     copyText: string;
@@ -13,6 +15,7 @@ const CopyButton = ({ copyText }: Props) => {
         <Button
             size="small"
             variant="outlined"
+            className={classes.btn}
             onClick={() => {
                 setIsShownLoading(true);
                 navigator.clipboard.writeText(copyText);
@@ -23,7 +26,8 @@ const CopyButton = ({ copyText }: Props) => {
                 }, 1000);
             }}
         >
-            {isShownLoading ? 'Copied' : 'Copy'}
+            <Image src={'/svg/copy.svg'} alt={'Copy'} width={18} height={18} />
+            <span>{isShownLoading ? 'Copied' : 'Copy'}</span>
         </Button>
     );
 };
