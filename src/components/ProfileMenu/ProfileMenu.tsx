42c483e1bcc0d24a62e1305f18feaecf61d15bc6
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-02 13:19:15 +0300

    feat: add container and background to the navbar

--- src/components/ProfileMenu/ProfileMenu.tsx
+++ src/components/ProfileMenu/ProfileMenu.tsx
@@ -43,88 +43,93 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({ userInfo, handlePopUp }) => {
     const isCounterMinus = Number(userInfo?.counter?.toFixed(0)) <= 0;
 
     return (
-        <div className={classes.profileMenu}>
-            <li className={classes.logo} onClick={() => push('/')}>
-                <Image
-                    priority={true}
-                    src={'/images/logo.png'}
-                    alt={'Logo'}
-                    width={150}
-                    height={45}
-                />
-            </li>
-            <li className={classes.li} onClick={handleHandleClickLi}>
-                <div className={classes.image_wrapper}>
-                    <>
-                        {user.photo ? (
+        <div className={clsx(classes.container, classes.navbarContainer)}>
+            <div className={classes.profileMenu}>
+                <li className={classes.logo} onClick={() => push('/')}>
+                    <Image
+                        priority={true}
+                        src={'/images/logo.png'}
+                        alt={'Logo'}
+                        width={150}
+                        height={45}
+                    />
+                </li>
+                <li className={classes.li} onClick={handleHandleClickLi}>
+                    <div className={classes.image_wrapper}>
+                        <>
+                            {user.photo ? (
+                                <Image
+                                    width={35}
+                                    height={35}
+                                    src={user.photo}
+                                    className={classes.profile}
+                                    alt="profile photo"
+                                />
+                            ) : (
+                                <UserPhotoPlaceholder userName={user.name} />
+                            )}
                             <Image
-                                width={35}
-                                height={35}
-                                src={user.photo}
-                                className={classes.profile}
-                                alt="profile photo"
+                                src={'/svg/settings.svg'}
+                                alt={'Settings'}
+                                width={25}
+                                height={25}
                             />
-                        ) : (
-                            <UserPhotoPlaceholder userName={user.name} />
-                        )}
-                        <Image
-                            src={'/svg/settings.svg'}
-                            alt={'Settings'}
-                            width={25}
-                            height={25}
-                        />
-                    </>
-                </div>
-                <ul
-                    className={clsx(classes.submenu, {
-                        [classes.submenuShown]: isSubmenuShown,
-                    })}
-                >
-                    <li
-                        className={clsx(classes.subItem, classes.bottomDivider)}
+                        </>
+                    </div>
+                    <ul
+                        className={clsx(classes.submenu, {
+                            [classes.submenuShown]: isSubmenuShown,
+                        })}
                     >
-                        <p className={classes.semibold}>
-                            {user.email || user.name}
-                        </p>
-                    </li>
-                    <li
-                        className={clsx(
-                            classes.subItem,
-                            classes.bottomDivider,
-                            isCounterMinus && classes.freeChecks
-                        )}
-                        onClick={() => isCounterMinus && handlePopUp()}
-                    >
-                        <Chip
-                            label={
-                                isCounterMinus
-                                    ? '0'
-                                    : String(userInfo?.counter?.toFixed(0))
-                            }
-                            className={classes.checksChip}
-                        />
-                        <p>Free checks</p>
-                    </li>
-                    <li
-                        className={classes.subItem}
-                        onClick={() => isCounterMinus && handlePopUp()}
-                    >
-                        <IconCreditCard color="#091F3D" size={20} />
-                        <p>Subscription</p>
-                    </li>
-                    <li
-                        className={classes.subItem}
-                        onClick={() => push('/history')}
-                    >
-                        <IconHistory color="#091F3D" size={20} />
-                        <p>History</p>
-                    </li>
-                    <li className={classes.subItem} onClick={handleLogout}>
-                        <IconLogout color="#091F3D" size={20} />
-                        <p>Logout</p>
-                    </li>
-                </ul>
-            </li>
+                        <li
+                            className={clsx(
+                                classes.subItem,
+                                classes.bottomDivider
+                            )}
+                        >
+                            <p className={classes.semibold}>
+                                {user.email || user.name}
+                            </p>
+                        </li>
+                        <li
+                            className={clsx(
+                                classes.subItem,
+                                classes.bottomDivider,
+                                isCounterMinus && classes.freeChecks
+                            )}
+                            onClick={() => isCounterMinus && handlePopUp()}
+                        >
+                            <Chip
+                                label={
+                                    isCounterMinus
+                                        ? '0'
+                                        : String(userInfo?.counter?.toFixed(0))
+                                }
+                                className={classes.checksChip}
+                            />
+                            <p>Free checks</p>
+                        </li>
+                        <li
+                            className={classes.subItem}
+                            onClick={() => isCounterMinus && handlePopUp()}
+                        >
+                            <IconCreditCard color="#091F3D" size={20} />
+                            <p>Subscription</p>
+                        </li>
+                        <li
+                            className={classes.subItem}
+                            onClick={() => push('/history')}
+                        >
+                            <IconHistory color="#091F3D" size={20} />
+                            <p>History</p>
+                        </li>
+                        <li className={classes.subItem} onClick={handleLogout}>
+                            <IconLogout color="#091F3D" size={20} />
+                            <p>Logout</p>
+                        </li>
+                    </ul>
+                </li>
+            </div>
         </div>
     );
 };
