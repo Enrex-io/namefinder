23851c8bdf6bfabe5241ca1c1aec928b8b14a37d
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2023-06-02 12:35:14 +0300

    feat: add clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
 
 export default function Document() {
+    // @ts-ignore
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "hdhrjdacjc");`,
+                    }}
+                />
+                ;
+            </Head>
             <body>
                 <Main />
                 <NextScript />
