189646210ccd89213e7c3010ebd9d25f9537bbf7
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2023-06-07 09:57:50 +0300

    feat: add new design to history and add layout

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,170 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { IGreenWashingUser, IPrompt } from '@/types';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { IPrompt } from '@/types';
 import Stack from '@/components/Stack/Stack';
 import classes from './history.module.scss';
 import useAuth from '@/hooks/useAuth';
 import { OpenAIApi } from '@/services/OpenAIService';
 import Paper from '@/components/Paper/Paper';
-import NavBar from '@/components/NavBar/NavBar';
-import { GreenWashingUserService } from '@/services/GreenWashingUserService';
-import Head from 'next/head';
-import { META } from '@/consts/meta';
-import PopUp from '@/components/PopUp/PopUp';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
+import Medias from '@/consts/medias';
+import Regions from '@/consts/region';
 import Button from '@/components/Button/Button';
 import Image from 'next/image';
+import Layout from '@/pages/layout';
+import SustainabilityDescription from '@/widgets/SustainabilityDescriptions/SustainabilityDescriptions';
+import MediaPost from '@/widgets/MediaPost/MediaPost';
 
-export default function History() {
-    const HEADING_TEXT = 'History';
-    const [userInfo, setUserInfo] = useState<IGreenWashingUser | null>(null);
-    const [history, setHistory] = useState<Array<IPrompt>>([]);
+function History() {
     const { user } = useAuth();
-    const [openPopUp, setOpenPopUp] = useState<boolean>(false);
+    const HEADING_TEXT = 'History';
     const SUBMIT_BUTTON_TEXT = 'Check post';
-
-    const handlePopUp = () => {
-        setOpenPopUp(!openPopUp);
-    };
-    useEffect(() => {
-        async function fetchUser() {
-            const userData = await GreenWashingUserService.getUser();
-            if (userData.result) {
-                setUserInfo(userData.result);
-                return;
-            }
-        }
-
-        if (user) {
-            fetchUser();
-        }
-    }, [user]);
+    const [history, setHistory] = useState<IPrompt>({
+        userId: '',
+        media: Medias.TwitterEU,
+        region: Regions.EU,
+        request: '',
+        unparsedResponse: '',
+        response: {
+            terms: [''],
+            correctText: '',
+        },
+        date: '',
+    });
+    const { query } = useRouter();
     useEffect(() => {
         const getHistory = async () => {
             if (!user) return;
 
             const data = await OpenAIApi.getHistory(user.id);
-            setHistory(data);
+            const historyOrder = data[Number(query?.order) as number];
+            setHistory(historyOrder);
+            return historyOrder;
         };
 
-        getHistory();
-    }, [user]);
+        getHistory().then((r) => console.log(r));
+    }, [query?.order, user]);
+
+    const date = history && new Date(history!.date as unknown as Date);
+    const dateText =
+        history && `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`;
     return (
         <>
-            <Head>
-                <title>{META.title}</title>
-                <link rel="icon" href={META.favicon} />
-                <meta name="description" content={META.description} />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-            </Head>
-            {openPopUp && <PopUp handlePopUp={handlePopUp} />}
-            <NavBar userInfo={userInfo} handlePopUp={handlePopUp} />
-            <Stack
-                className={classes.container}
-                spacing={1.25}
-                direction="column"
-            >
-                <h2 className={classes.heading}>{HEADING_TEXT}</h2>
+            <Stack className={classes.container} direction="column">
                 <Stack direction="column" spacing={1.25}>
-                    {history && history?.[0] ? (
-                        history.map((prompt: IPrompt, index: number) => {
-                            const date: Date = new Date(
-                                prompt!.date as unknown as Date
-                            );
-                            const dateText = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} - ${
-                                prompt.media
-                            }`;
-
-                            return (
-                                <>
-                                    <Paper
-                                        className={classes.paper}
-                                        direction="column"
-                                        spacing={1.5}
-                                        hasBorder
-                                    >
-                                        <h3 className={classes.goalHeading}>
-                                            <span>{dateText}</span>
-                                        </h3>
-                                        <Paper
-                                            className={classes.paperPrompt}
-                                            direction="column"
-                                            spacing={1}
-                                            hasBorder
-                                        >
-                                            <h3
-                                                className={
-                                                    classes.goalDescription
-                                                }
-                                            >
-                                                <span></span>
-                                                {prompt.request}
-                                            </h3>
-                                        </Paper>
-                                        {prompt.response.terms.map(
-                                            (term: string) => {
-                                                return (
-                                                    <Paper
-                                                        key={term}
-                                                        className={
-                                                            classes.paperDescription
-                                                        }
-                                                        direction="row"
-                                                    >
-                                                        <p
-                                                            className={
-                                                                classes.goalDescription
-                                                            }
-                                                        >
-                                                            <span>{term}</span>
-                                                        </p>
-                                                    </Paper>
-                                                );
-                                            }
-                                        )}
-                                        <Paper
-                                            className={classes.paperPost}
-                                            direction="column"
-                                            spacing={1}
-                                            hasBorder
-                                        >
-                                            <div className={classes.post}>
-                                                <div
-                                                    className={
-                                                        classes.avatar_wrapper
-                                                    }
-                                                >
-                                                    <div
-                                                        className={classes.logo}
-                                                    ></div>
-                                                    <div
-                                                        className={
-                                                            classes.nickname
-                                                        }
-                                                    >
-                                                        @nickname
-                                                    </div>
-                                                </div>
-                                                <div
-                                                    className={
-                                                        classes.description
-                                                    }
-                                                >
-                                                    <p>
-                                                        {
-                                                            prompt.response
-                                                                .correctText
-                                                        }
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </Paper>
-                                    </Paper>
-                                </>
-                            );
-                        })
+                    {history ? (
+                        <>
+                            <Paper
+                                className={classes.paper}
+                                direction="column"
+                                hasBorder
+                            >
+                                <h3 className={classes.goalHeading}>
+                                    <span>Your post {dateText}</span>
+                                </h3>
+                                <Paper
+                                    className={classes.paperPrompt}
+                                    direction="column"
+                                    spacing={1}
+                                    hasBorder
+                                >
+                                    <h3 className={classes.goalDescription}>
+                                        <span></span>
+                                        {history.request}
+                                    </h3>
+                                </Paper>
+                            </Paper>
+                            {history.response.terms[0] && (
+                                <SustainabilityDescription
+                                    descriptions={history.response.terms}
+                                />
+                            )}
+                            {history.response.correctText && (
+                                <MediaPost
+                                    media={history.media || ''}
+                                    post={history.response.correctText}
+                                />
+                            )}
+                        </>
                     ) : (
                         <>
                             <Paper
@@ -212,3 +133,9 @@ export default function History() {
         </>
     );
 }
+
+History.getLayout = function getLayout(page: ReactElement) {
+    return <Layout>{page}</Layout>;
+};
+
+export default History;
