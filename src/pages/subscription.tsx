a9967680b46273a1fcd64343c3881f789df8ca6d
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-08 14:26:13 +0300

    fix: fix the Subscription page according to new layout

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -1,59 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { IGreenWashingUser } from '@/types';
+import React, { ReactElement } from 'react';
 import classes from './subscription.module.scss';
 import useAuth from '@/hooks/useAuth';
-import NavBar from '@/components/NavBar/NavBar';
-import { GreenWashingUserService } from '@/services/GreenWashingUserService';
-import Head from 'next/head';
-import { META } from '@/consts/meta';
-import PopUp from '@/components/PopUp/PopUp';
 import Script from 'next/script';
 import Stack from '@/components/Stack/Stack';
-import AuthGuard from '@/utils/route-guard/AuthGuard';
+import Layout from '@/pages/layout';
 
 const HEADING_TEXT = 'Subscription';
 const STRIPE_PRICING_TABLE_ID = process.env.NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID;
 const STRIPE_PUBLISHABLE_ID = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_ID;
 
-export default function Subscription() {
-    const [userInfo, setUserInfo] = useState<IGreenWashingUser | null>(null);
+const Subscription = () => {
     const { user } = useAuth();
-    const [openPopUp, setOpenPopUp] = useState<boolean>(false);
-
-    const handlePopUp = () => {
-        setOpenPopUp(!openPopUp);
-    };
-    useEffect(() => {
-        async function fetchUser() {
-            const userData = await GreenWashingUserService.getUser();
-            if (userData.result) {
-                setUserInfo(userData.result);
-                return;
-            }
-        }
-
-        if (user) {
-            fetchUser();
-        }
-    }, [user]);
 
     return (
-        <AuthGuard>
-            <Head>
-                <title>{META.title}</title>
-                <link rel="icon" href={META.favicon} />
-                <meta name="description" content={META.description} />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-            </Head>
+        <>
             <Script
                 strategy="afterInteractive"
                 src="https://js.stripe.com/v3/pricing-table.js"
             />
-            {openPopUp && <PopUp handlePopUp={handlePopUp} />}
-            <NavBar userInfo={userInfo} />
             <Stack
                 className={classes.container}
                 spacing={1.25}
@@ -66,6 +30,12 @@ export default function Subscription() {
                     }}
                 ></div>
             </Stack>
-        </AuthGuard>
+        </>
     );
-}
+};
+
+Subscription.getLayout = function getLayout(page: ReactElement) {
+    return <Layout>{page}</Layout>;
+};
+
+export default Subscription;
