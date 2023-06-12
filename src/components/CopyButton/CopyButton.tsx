8ec593e22dda11925cf38d81d6db2c396b99c14b
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2023-06-12 18:06:24 +0300

    feat: add copy on helperCopy

--- src/components/CopyButton/CopyButton.tsx
+++ src/components/CopyButton/CopyButton.tsx
@@ -11,20 +11,22 @@ const CopyButton = ({ copyText }: Props) => {
     const [isShownLoading, setIsShownLoading] = useState<boolean>(false);
     const currentTimeout = useRef<number>();
 
+    const helperCopy = () => {
+        setIsShownLoading(true);
+        navigator.clipboard.writeText(copyText);
+        if (currentTimeout.current) window.clearTimeout(currentTimeout.current);
+        currentTimeout.current = window.setTimeout(() => {
+            setIsShownLoading(false);
+        }, 1000);
+    };
+
     return (
         <Button
             size="small"
             variant="outlined"
             className={classes.btn}
-            onClick={() => {
-                setIsShownLoading(true);
-                navigator.clipboard.writeText(copyText);
-                if (currentTimeout.current)
-                    window.clearTimeout(currentTimeout.current);
-                currentTimeout.current = window.setTimeout(() => {
-                    setIsShownLoading(false);
-                }, 1000);
-            }}
+            onTouchStart={helperCopy}
+            onClick={helperCopy}
         >
             <Image src={'/svg/copy.svg'} alt={'Copy'} width={18} height={18} />
             <span>{isShownLoading ? 'Copied' : 'Copy'}</span>
