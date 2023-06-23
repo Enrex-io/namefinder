718e21c7661727210465fb845113ef5cfebc3051
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-23 09:34:42 +0300

    feat: separate and style the PricingTable component

---
+++ src/components/Subscription/PricingTable/PricingTable.tsx
@@ -0,0 +1,37 @@
+import Stack from '@/components/Stack/Stack';
+import Script from 'next/script';
+import React, { FC } from 'react';
+import classes from './PricingTable.module.scss';
+
+interface PricingTableProps {
+    headingText: string;
+    userId?: string;
+}
+
+const STRIPE_PRICING_TABLE_ID = process.env.NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID;
+const STRIPE_PUBLISHABLE_ID = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_ID;
+
+const PricingTable: FC<PricingTableProps> = ({ headingText, userId }) => {
+    return (
+        <>
+            <Script
+                strategy="afterInteractive"
+                src="https://js.stripe.com/v3/pricing-table.js"
+            />
+            <Stack
+                className={classes.container}
+                spacing={1.25}
+                direction="column"
+            >
+                <h2 className={classes.heading}>{headingText}</h2>
+                <div
+                    dangerouslySetInnerHTML={{
+                        __html: `<stripe-pricing-table pricing-table-id=${STRIPE_PRICING_TABLE_ID} publishable-key="${STRIPE_PUBLISHABLE_ID}" client-reference-id="${userId}"></stripe-pricing-table>`,
+                    }}
+                ></div>
+            </Stack>
+        </>
+    );
+};
+
+export default PricingTable;
--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -1,18 +1,14 @@
 import React, { ReactElement } from 'react';
-import classes from './subscription.module.scss';
 import useAuth from '@/hooks/useAuth';
-import Script from 'next/script';
-import Stack from '@/components/Stack/Stack';
 import Layout from '@/pages/layout';
 import useSWR from 'swr';
 import axios from '@/utils/axios';
 import { IGreenWashingUser } from '@/types';
 import FullscreenLoader from '@/components/Loader/FullscreenLoader';
 import ActiveSubscription from '@/components/Subscription/ActiveSubscription/ActiveSubscription';
+import PricingTable from '@/components/Subscription/PricingTable/PricingTable';
 
 const HEADING_TEXT = 'Subscription';
-const STRIPE_PRICING_TABLE_ID = process.env.NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID;
-const STRIPE_PUBLISHABLE_ID = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_ID;
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const Subscription = () => {
@@ -40,26 +36,7 @@ const Subscription = () => {
         return <ActiveSubscription headingText={HEADING_TEXT} />;
     }
 
-    return (
-        <>
-            <Script
-                strategy="afterInteractive"
-                src="https://js.stripe.com/v3/pricing-table.js"
-            />
-            <Stack
-                className={classes.container}
-                spacing={1.25}
-                direction="column"
-            >
-                <h2 className={classes.heading}>{HEADING_TEXT}</h2>
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: `<stripe-pricing-table pricing-table-id=${STRIPE_PRICING_TABLE_ID} publishable-key="${STRIPE_PUBLISHABLE_ID}" client-reference-id="${user?.id}"></stripe-pricing-table>`,
-                    }}
-                ></div>
-            </Stack>
-        </>
-    );
+    return <PricingTable headingText={HEADING_TEXT} userId={user?.id} />;
 };
 
 Subscription.getLayout = function getLayout(page: ReactElement) {
